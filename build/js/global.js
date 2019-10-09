a8ec91b2fcd281e232dcecb689a1cf726a736b1d
Adam Parker <adam@theadamparker.com>
2019-10-09 14:41:13 -0400

    🎹

--- build/js/global.js
+++ build/js/global.js
@@ -16,27 +16,42 @@ window.addEventListener('keydown', handleFirstTab);
 
 function handleNext() {
 
-  var $radios = $("input[class*='slide-radio']");
-  var $activeRadio = $("input[class*='slide-radio']:checked");
+  var $radios = $('input[class*="slide-radio"]');
+  var $activeRadio = $('input[class*="slide-radio"]:checked');
 
   var currentIndex = $activeRadio.index();
   var radiosLength = $radios.length;
 
+  console.log("currentIndex: " + currentIndex);
+  console.log("radiosLength: " + radiosLength);
+
   $radios
     .attr('checked', false);
+
   if (currentIndex >= radiosLength - 1) {
+
     $radios
       .first()
       .attr('checked', true);
+      // console.log("if true");
+
   } else {
+
     $activeRadio
-      .next("input[class*='slide-radio']")
+      .next('input[class*="slide-radio"]')
       .attr('checked', true);
+      // console.log("else true");
   }
+
 }
 
 $('.controls__button--next').click(function() {
   handleNext()
+  console.log("click")
 });
 
-// $('.controls__button--next').click(handleNext());
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+$(document).ready(function() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
